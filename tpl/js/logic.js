f3259bf3c543a64ae66854ee2a7a1b2a70da91a7
Giovanni De Toni <giovanni.det@gmail.com>
2016-11-14 10:19:42 +0100

    Add input fields to order a new item.
    Add also javascript methods to display and hide these
    new input fields.

---
+++ tpl/js/logic.js
@@ -0,0 +1,11 @@
+// View methods
+
+// Hide the input to add a new item
+function hideAddOrder() {
+  document.getElementById("new_item").style.visibility="hidden";
+}
+
+// Display the input fields to add a new item
+function displayAddOrder() {
+  document.getElementById("new_item").style.visibility="visible";
+}
